a0022159f32a4760501bb88cf4fd9fc3c73bb761
Conrad Benham <cbenham@gmail.com>
2011-08-28 16:59:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quiz::Application.config.secret_token = 'f010bf9eb11575ee199fb285018b28a243e0c7b42195ae5fcf30b45be9d86f139af58505c4fa70fb8bafa9fa91169808dc304e166b97c3bbfa05323ca4c57720'
