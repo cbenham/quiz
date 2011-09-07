92c381e9d3eb67403900c96a584bf9eaa64084a5
Conrad Benham <cbenham@gmail.com>
2011-09-06 22:58:22 -0400

    CREATE QUESTION FORM
    Form to create a new question and associated answers.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,16 @@
 class QuestionsController < ApplicationController
 
+  respond_to :html
+
   def index
     @questions = Question.all
+    respond_with @questions
+  end
+
+  def new
+    @question = Question.new
+    4.times { @question.answers.build }
+    respond_with @question
   end
 
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -6,7 +6,7 @@ describe QuestionsController do
       {:get => '/questions'}.should route_to(:controller => 'questions', :action => 'index')
     end
 
-    it 'should show a list of questions that have been entered' do
+    it 'show a list of questions that have been entered' do
       answers = %w{a b c d}.collect { |letter| Factory(:answer, :position => letter, :answer => "Answer: #{letter}") }
       Factory(:question, :question => "First letter of the alphabet?", :answers => answers,
               :choice => answers.first)
@@ -20,6 +20,18 @@ describe QuestionsController do
       response.should be_success
       response.body.should =~ /First letter of the alphabet?/m
       response.body.should =~ /Second letter of the alphabet?/m
+    end
+
+    it 'show the user a form used to create a question and associated answers' do
+      get :new
+
+      response.should be_success
+      response.body.should =~ /New Question/m
+      response.body.should =~ /Question:/m
+      4.times do |i|
+        response.body.should =~ /#{i + 1}:/m
+      end
+      response.body.should =~ /Create Question/m
     end
   end
 end
