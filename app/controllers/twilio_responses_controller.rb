922caef047f4c0dbd56ea55c88db8cdf6fb14c72
Conrad Benham <cbenham@gmail.com>
2011-09-11 23:33:29 -0400

    Remove whitespace from SMS body

--- app/controllers/twilio_responses_controller.rb
+++ app/controllers/twilio_responses_controller.rb
@@ -1,7 +1,7 @@
 class TwilioResponsesController < ApplicationController
 
   def create
-    answer = session[:current_question].answers.find_by_answer(params[:Body])
+    answer = session[:current_question].answers.find_by_answer(params[:Body].strip)
     answer.numbers.build(:number => params[:From])
     answer.save!
   end
--- spec/controllers/twilio_responses_controller_spec.rb
+++ spec/controllers/twilio_responses_controller_spec.rb
@@ -2,20 +2,38 @@ require 'spec_helper'
 
 describe TwilioResponsesController do
 
-  it 'should associate an incoming SMS message as an answer with the current question' do
-    question = Factory(:question)
-    session[:current_question] = question
+  context 'with a current question' do
 
-    assert_difference 'Number.count' do
-      post :create, :From => '1234567890', :To => '0987654321', :Body => '1'
+    before(:each) do
+      @question = Factory(:question)
+      session[:current_question] = @question
     end
 
-    number = Number.first
-    number.answers.count.should eql(1)
-    number.number.should eql('1234567890')
-    number.answers.should eql([question.answers.first])
+    it 'should associate an incoming SMS message as an answer with the current question' do
+      assert_difference 'Number.count' do
+        post :create, :From => '1234567890', :To => '0987654321', :Body => '1'
+      end
 
-    question.answers.find_by_answer('1').numbers.should eql([number])
+      number = Number.first
+      number.answers.count.should eql(1)
+      number.number.should eql('1234567890')
+      number.answers.should eql([@question.answers.first])
+
+      @question.answers.find_by_answer('1').numbers.should eql([number])
+    end
+
+    it 'should remove any whitespace in an answer' do
+      assert_difference 'Number.count' do
+        post :create, :From => '1234567890', :To => '0987654321', :Body => ' 1 '
+      end
+
+      number = Number.first
+      number.answers.count.should eql(1)
+      number.number.should eql('1234567890')
+      number.answers.should eql([@question.answers.first])
+
+      @question.answers.find_by_answer('1').numbers.should eql([number])
+    end
   end
 
 end
