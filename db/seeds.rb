e8e7785d5d744dda5f04d358b5aeee83c5a558bb
Conrad Benham <cbenham@gmail.com>
2011-09-14 17:55:32 -0400

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-answers = [actual_answer = Answer.new(:answer => 10, :position => 1)]
-answers << Answer.new(:answer => 20, :position => 2)
-answers << Answer.new(:answer => 30, :position => 3)
-answers << Answer.new(:answer => 40, :position => 4)
-Question.create!(:question => 'What is 10 + 10?', :answers => answers, :choice => actual_answer)
+answers = [Answer.new(:answer => 2010, :position => 1),
+           Answer.new(:answer => 2011, :position => 2),
+           Answer.new(:answer => 2012, :position => 3),
+           Answer.new(:answer => 2013, :position => 4)]
+Question.create!(:question => 'What year is it?', :answers => answers, :user_choice => 1)
